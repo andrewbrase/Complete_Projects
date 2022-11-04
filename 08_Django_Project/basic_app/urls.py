a73ca2bb43b1e0433ccb6f6caa1c8afcbd0b71a2
Brase <brasea@wwt.com>
2022-11-04 10:27:44 -0500

    moved over user auth model django project

---
+++ 08_Django_Project/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from basic_app import views
+
+# template urls app name variable
+app_name = 'basic_app'
+
+urlpatterns=[ 
+    path('register/',views.register,name='register'),
+]
